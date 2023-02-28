949f47ee1b15e754bdcb73f754dbc645c1d3c154
Caio Braga <caiotabraga@hotmail.com>
2023-02-28 15:42:45 -0300

    Testing

--- cLinear.java
+++ cLinear.java
@@ -2,4 +2,20 @@ package MN;
 
 public class cLinear {
     
+    private x xAxis;
+    private y yAxis;
+    private summation sum;
+    private cAngular ca;
+
+    public cLinear(x x, y y){
+        this.xAxis = x;
+        this.yAxis = y;
+        sum = new summation(x, y);
+        ca = new cAngular(x, y);
+    }
+    
+    public double calculateCL(){
+        return ((sum.sumAxisY()) / xAxis.getLength()) - ((ca.calculateCA() * sum.sumAxisX()) / xAxis.getLength());
+    }
+
 }
--- menu.java
+++ menu.java
@@ -10,48 +10,53 @@ public class menu {
         y yAxis;
         Double value;
         cAngular ca;
+        cLinear cl;
         do {
             menu();
             op = in.nextInt();
-            while (op != 1 && op != 2 && op != 3 && op != 4 && op != 5) {
+            while (op != 1 && op != 2 && op != 3) {
                 System.out.println("Invalid option! Please insert a valid one:");
                 op = in.nextInt();
             }
-        switch (op) {
-            case 1:
-            System.out.println("Insert x and y axis element´s length: ");
-            length = in.nextInt();
-            xAxis = new x(length);
-            yAxis = new y(length);
-            for(int i = 0; i < length; i++){
-                System.out.print("Insert the value of x axis on xAxis["+i+"]: ");
-                value = in.nextDouble();
-                xAxis.addValue(value);
-                System.out.print("Insert the value of y axis on yAxis["+i+"]: ");
-                value = in.nextDouble();
-                yAxis.addValue(value);
+            switch (op) {
+                case 1:
+                    System.out.println("Insert x and y axis element´s length: ");
+                    length = in.nextInt();
+                    xAxis = new x(length);
+                    yAxis = new y(length);
+                    for (int i = 0; i < length; i++) {
+                        System.out.print("Insert the value of x axis on xAxis[" + i + "]: ");
+                        value = in.nextDouble();
+                        xAxis.addValue(value);
+                        System.out.print("Insert the value of y axis on yAxis[" + i + "]: ");
+                        value = in.nextDouble();
+                        yAxis.addValue(value);
+                    }
+                    ca = new cAngular(xAxis, yAxis);
+                    cl = new cLinear(xAxis, yAxis);
+                    for(int j = 0; j < length; j++){
+                        System.out.println("X["+j+"] :" +xAxis.getValue(j)+ "Y["+j+"] : " +yAxis.getValue(j));
+                    }
+                    System.out.printf("Regression Linear value : Y = %a x X + %l", ca.calculateCA(), cl.calculateCL());
+                    break;
+                case 2:
+
+                   
+                    break;
+                case 3:
+
+                    break;
+                // ...
+                default:
+                    // código a ser executado quando nenhum dos casos anteriores é satisfeito
             }
-            ca = new cAngular(xAxis, yAxis);
-            break;
-            case 2:
-            
-            break;
-            case 3:
-                
-                break;
-            // ...
-            default:
-                // código a ser executado quando nenhum dos casos anteriores é satisfeito
-        }
-    }while(op != 3);
-        
+        } while (op != 3);
+
     }
 
     public static void menu() {
         System.out.println("Linear regression calculation:");
-        System.out.println("1 - Add values to X axis");
-        System.out.println("2 - Add values to Y axis");
-        System.out.println("3 - Linear regression line value");
-        System.out.println("4 - Linear correlation coefficient");
+        System.out.println("1 - Add values to X and Y axis to calculate Linear regression line");
+        System.out.println("2 - Linear correlation coefficient");
     }
 }
